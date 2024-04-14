a38b83c788c74834476d445cb7d24a4d248bec51
Alexey Svistunov <xdayx53@gmail.com>
2024-04-14 16:51:42 +0300

    add some styles for toggle(active - green)

--- src/main.jsx
+++ src/main.jsx
@@ -16,16 +16,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 
 
 
-// edit-team
-// нужно чтобы список участников писался
-// другие аватарки
-// nextTurn, плохо работает nextTurn, ему нужна внутренняя логика
-// когда все ready, формируется массив неповторяющихся слов с предыдущей командой(это для второй и так далее команды), а так просто тусуются из api или из массива
-// победитель
-// какие-то флаги, запущена игра или нет, чтобы нельзя было переходить по ненужным роутам
-
-// wrong score
-// delete in createTeamPage
-// result checkbox functionality
-// score has already been and It's 9 before I changed it
-// winner check (if there's a winner - button play again and everything is gonna be again with the same team or what you choose)
+// аватарки
+// цвета
+// логика победителя
+// запрет роутов, возможно по флагам
+// localStorage
